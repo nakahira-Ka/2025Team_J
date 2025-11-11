c609f68d39324f2f3c0d3005e43a26794ef36da6
nakahira-Ka <sabu1wawa@icloud.com>
2025-11-12 00:41:46 +0900

    名称変更と画面遷移

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
-import Map from "./pages/Map";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/map" element={<Map />} />
+        <Route path="/" element={<Login />} /> 
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
 };
+
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="top">
-      <Link to="/map">
-        <h1>aaaaa</h1>
+      <Link to="/home">
+        <h1>aaaaa11111</h1>
       </Link>
     </div>
   );
